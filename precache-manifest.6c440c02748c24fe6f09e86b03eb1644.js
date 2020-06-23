e1811e6ba592baf87c05340903ea52afb576ee4a
gokul <gokulakrishnan.312@gmail.com>
2020-06-24 00:36:40 +0530

    Updates

---
+++ precache-manifest.6c440c02748c24fe6f09e86b03eb1644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a004dea0b2d43ce13816436ecf9f2d",
+    "url": "/my-first-reactApp-toDoList/index.html"
+  },
+  {
+    "revision": "f367652820a41ce038f2",
+    "url": "/my-first-reactApp-toDoList/static/css/main.4f5cb33a.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/my-first-reactApp-toDoList/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-reactApp-toDoList/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f367652820a41ce038f2",
+    "url": "/my-first-reactApp-toDoList/static/js/main.9683ba4a.chunk.js"
+  },
+  {
+    "revision": "44c667ce2f4584fd6467",
+    "url": "/my-first-reactApp-toDoList/static/js/runtime-main.80339f57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-reactApp-toDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
